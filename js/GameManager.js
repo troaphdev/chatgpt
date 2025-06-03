51e56d7a049c1b1a232775c530aff4bd3a84c2d2
Troaph <143641860+troaphdev@users.noreply.github.com>
2025-06-03 19:58:25 -0400

    Fix module import for PointerLockControls

--- js/GameManager.js
+++ js/GameManager.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.155.0/build/three.module.js';
-import { PointerLockControls } from 'https://unpkg.com/three@0.155.0/examples/jsm/controls/PointerLockControls.js';
+import { PointerLockControls } from 'https://unpkg.com/three@0.155.0/examples/jsm/controls/PointerLockControls.js?module';
 import PhoneUI from './PhoneUI.js';
 import Events from './Events.js';
 
